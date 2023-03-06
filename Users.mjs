0ddcbde42d978aa209838f3f81a5546dfbf7de5a
Fabrizio_Schwindt <123416659+FabrizioSCH@users.noreply.github.com>
2023-03-06 11:42:33 -0300

    Create Users.mjs

---
+++ Users.mjs
@@ -0,0 +1,30 @@
+/**
+*  Users
+*  En esta clase alojan los datos y parametros para todos los usuarios
+*  @example const instance = new Object()
+**/
+
+class Users{
+
+    #name;
+    #surname;
+    #dni;
+    #mail;
+    #typeuser;
+ 
+    constructor(name, surname, dni, mail, permission){
+        this.#name;
+        this.#surname;
+        this.#dni;
+        this.#mail;
+        this.#typeuser;
+    }
+     
+     logIn(){};
+     logOut(){};
+     search(){};
+     call(){};
+     buy(){};
+     identify(){};
+ 
+ }
