9465eda4ac10c88a78805e8c929340b51d2c2acb
Fabrizio_Schwindt <123416659+FabrizioSCH@users.noreply.github.com>
2023-03-06 11:41:57 -0300

    Create Inventory.mjs

---
+++ Inventory.mjs
@@ -0,0 +1,29 @@
+/**
+*  Intenvory
+*  En esta clase se almacenan los vehiculos
+*  @example const instance = new Object()
+**/
+
+class Inventory{
+  
+    #invItem;
+    #invPlace;
+    #invNum;
+    #invSto;
+    #invSiz;
+  
+    constructor(item, place, number, stock, size){
+          this.#invItem;
+          this.#invPlace;
+          this.#invNum;
+          this.#invSto;
+          this.#invSiz;
+     }
+        
+     store(){};
+     identify(){};
+     itemAcces(){};
+     itemExtract(){};
+     itemList(){};
+     itemFilter(){};
+}
