845dfd572096019dfc0fe869ded53de17296d9d6
Fabrizio_Schwindt <123416659+FabrizioSCH@users.noreply.github.com>
2023-03-06 11:43:00 -0300

    Create Concessionaire.mjs

---
+++ Concessionaire.mjs
@@ -0,0 +1,26 @@
+/**
+* Concessionaire
+*  En esta clase el Usuario podrá interactuar
+*  @example const instance = new Object()
+**/
+
+
+class Concessionaire{
+  
+    #localName;
+    #localAddress;
+    #localEmployees;
+    #localSurface;
+
+    constructor(name, address, employees, surface){
+        this.#localName;
+        this.#localAddress;
+        this.#localEmployees;
+        this.#localSurface;
+        }
+
+    sell(){};
+    import(){};
+    export(){};
+    exhibit(){};
+}
