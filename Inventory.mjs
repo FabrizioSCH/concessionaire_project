d25cb468cfe7f6e15b6d6f84979e5d707da59156
Fabrizio_Schwindt <123416659+FabrizioSCH@users.noreply.github.com>
2023-03-14 10:51:46 -0300

    Update Inventory.mjs

--- Inventory.mjs
+++ Inventory.mjs
@@ -4,6 +4,28 @@
 *  @example const instance = new Object()
 **/
 
+import {Automoviles} from './Array.mjs'
+
+
+
+export const autos = new Automoviles();
+    autos.agregarMarca('toyota', 'corolla', 'nafta', 'auto', 1.8, 'sedan', 'azul', 2022, 50000);
+    console.log(autos.automoviles); // Devuelve el array automoviles con el nuevo auto añadido 
+
+export const automovil = new Automoviles();
+    const autosFiltrados = automovil.filtro('chevrolet', 'nafta');
+    console.log(autosFiltrados); // Muestra un array con los automoviles filtrados
+
+export const autoss = new Automoviles();
+    autoss.agregarAuto('lamborghini', 'murcielago', 'nafta', 'manual', 6.5, 'coupe', 'negro', 2011, 100000);
+    console.log(autoss.automoviles); // Devuelve un array automoviles con el nuevo auto añadido
+
+
+
+
+
+/*
+
 class Inventory{
   
     #invItem;
@@ -27,3 +49,5 @@ class Inventory{
      itemList(){};
      itemFilter(){};
 }
+
+*/
