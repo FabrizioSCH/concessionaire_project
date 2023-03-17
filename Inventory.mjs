4733b85a192536e6ad0d4f79f20b70b40390b20d
Fabrizio_Schwindt <123416659+FabrizioSCH@users.noreply.github.com>
2023-03-17 01:32:45 -0300

    Update Inventory.mjs

--- Inventory.mjs
+++ Inventory.mjs
@@ -1,53 +1,31 @@
-/**
-*  Intenvory
-*  En esta clase se almacenan los vehiculos
-*  @example const instance = new Object()
-**/
-
-import {Automoviles} from './Array.mjs'
-
-
-
-export const autos = new Automoviles();
-    autos.agregarMarca('toyota', 'corolla', 'nafta', 'auto', 1.8, 'sedan', 'azul', 2022, 50000);
-    console.log(autos.automoviles); // Devuelve el array automoviles con el nuevo auto añadido 
-
-export const automovil = new Automoviles();
-    const autosFiltrados = automovil.filtro('chevrolet', 'nafta');
-    console.log(autosFiltrados); // Muestra un array con los automoviles filtrados
-
-export const autoss = new Automoviles();
-    autoss.agregarAuto('lamborghini', 'murcielago', 'nafta', 'manual', 6.5, 'coupe', 'negro', 2011, 100000);
-    console.log(autoss.automoviles); // Devuelve un array automoviles con el nuevo auto añadido
-
-
-
-
-
 /*
+* Aqui se puede acceder a los métodos de la clase Brand e interactuar con ellos
 
-class Inventory{
-  
-    #invItem;
-    #invPlace;
-    #invNum;
-    #invSto;
-    #invSiz;
-  
-    constructor(item, place, number, stock, size){
-          this.#invItem;
-          this.#invPlace;
-          this.#invNum;
-          this.#invSto;
-          this.#invSiz;
-     }
-        
-     store(){};
-     identify(){};
-     itemAcces(){};
-     itemExtract(){};
-     itemList(){};
-     itemFilter(){};
-}
-
+* Se requiere crear una clase inventory y ubicar todo el código dentro de ella
 */
+
+
+import { Brand } from './brand.mjs'
+
+
+export const newPeugeot = new Brand();
+    newPeugeot.addPeugeot("208", 2021, "nafta", "blanco", "manual", 25000);
+    console.log(newPeugeot.peugeot); // Devuelve un array peugeot nuevo con el auto añadido // ok
+
+
+export const newChevrolet = new Brand();
+    newChevrolet.addChevrolet("Camaro", 2022, "nafta", "rojo", "manual", 50000);
+    console.log(newChevrolet.chevrolet); // Devuelve un array chevrolet nuevo con el auto añadido // ok
+
+
+export const newBrand = new Brand();
+    newBrand.addBrand("Toyota");
+    console.log(newBrand.toyota); // Añade al constructor un nuevo objeto con el nombre de la marca a añadir la cual contiene un arreglo vacio 
+
+
+export const carFilter = new Brand();
+    const filteredCars = carFilter.filterByProperty("year", 2023);
+    console.log(filteredCars); // Devuelve un arreglo con los autos que coincidan con los datos añadidos // No especifica la marca >> hay que especificar el tipo de propiedad que se quiere busacar y su valor 
+
+export const myBrand = new Brand();
+    console.log(myBrand.listInventory()); // Devuelve un array con todos los automoviles disponibles en el inventario
