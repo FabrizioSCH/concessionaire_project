c7a1931c1141dd65e3ba1a01c5e770262869054f
Fabrizio_Schwindt <123416659+FabrizioSCH@users.noreply.github.com>
2023-03-17 01:36:38 -0300

    Update and rename Concessionaire.mjs to concessionaire.mjs

---
+++ concessionaire.mjs
@@ -0,0 +1,26 @@
+/**
+* Concessionaire
+*  En esta clase el Usuario podrá interactuar
+*  @example const instance = new Object()
+**/
+
+
+class Concessionaire{
+  
+    #name;
+    #address;
+    #employees;
+    #surface;
+
+    constructor(local, location, numberEmployees, meters){
+        this.#name;
+        this.#address;
+        this.#employees;
+        this.#surface;
+        }
+
+    sell(){};
+    import(){};
+    export(){};
+    exhibit(){};
+}
