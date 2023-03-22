48e323aa2d9074e63d5de52166b9656f71afeaf5
Fabrizio_Schwindt <123416659+FabrizioSCH@users.noreply.github.com>
2023-03-22 20:55:26 -0300

    Update interact.mjs

--- interact.mjs
+++ interact.mjs
@@ -1,31 +1,32 @@
 /*
-* Aqui se puede acceder a los métodos de la clase Brand e interactuar con ellos
-
-* Se requiere crear una clase inventory y ubicar todo el código dentro de ella
+* Aqui se puede acceder a los métodos de la clase Brands e interactuar con ellos
 */
 
 
-import { Brand } from './brand.mjs'
+import { Brands } from './brands.mjs'
 
 
-export const newPeugeot = new Brand();
-    newPeugeot.addPeugeot("208", 2021, "nafta", "blanco", "manual", 25000);
-    console.log(newPeugeot.peugeot); // Devuelve un array peugeot nuevo con el auto añadido // ok
+const brands = new Brands();
+
+brands.addBrand('Peugeot');
+brands.addBrand('Chevrolet');
+brands.addBrand('Toyota');
+
+brands.addCar('Peugeot', '5008', 2023, 'nafta', 'gris oscuro', 'auto', 55000);
+brands.addCar('Peugeot', '308', 2014, 'nafta', 'gris oscuro', 'manual', 44000);
+brands.addCar('Peugeot', '206', 2009, 'nafta', 'rojo', 'manual', 20000);
+brands.addCar('Peugeot', '408', 2014, 'diesel', 'blanco', 'manual', 40000);
+
+brands.addCar('Chevrolet', 'tracker', 2023, 'nafta', 'gris', 'auto', 47000);
+brands.addCar('Chevrolet', 'onix', 2023, 'nafta', 'gris', 'manual', 39000);
+brands.addCar('Chevrolet', 's10', 2023, 'diesel', 'azul', 'auto', 42000);
+brands.addCar('Chevrolet', 'corsa', 2009, 'nafta', 'rojo', 'manual', 19000);
+
+brands.addCar('Toyota', 'Prius', 2018, 'nafta', 'blanco', 'auto', 40000)
+
+console.log(brands);
 
 
-export const newChevrolet = new Brand();
-    newChevrolet.addChevrolet("Camaro", 2022, "nafta", "rojo", "manual", 50000);
-    console.log(newChevrolet.chevrolet); // Devuelve un array chevrolet nuevo con el auto añadido // ok
+console.log(brands.filterCars('Peugeot', '308'));
+console.log(brands.filterCars('Chevrolet', 'tracker'));
 
-
-export const newBrand = new Brand();
-    newBrand.addBrand("Toyota");
-    console.log(newBrand.toyota); // Añade al constructor un nuevo objeto con el nombre de la marca a añadir la cual contiene un arreglo vacio 
-
-
-export const carFilter = new Brand();
-    const filteredCars = carFilter.filterByProperty("year", 2023);
-    console.log(filteredCars); // Devuelve un arreglo con los autos que coincidan con los datos añadidos // No especifica la marca >> hay que especificar el tipo de propiedad que se quiere busacar y su valor 
-
-export const myBrand = new Brand();
-    console.log(myBrand.listInventory()); // Devuelve un array con todos los automoviles disponibles en el inventario
